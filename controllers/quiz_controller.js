e2ea7751b184ca70bc61ff061d4690e37256aaab
fjvalverdep <fjvalverdep@hotmail.com>
2015-08-03 18:32:50 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -60,6 +60,29 @@ exports.create = function(req, res) {
     });
 };
 
+//GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz;	//autoload de instancia de quiz
+
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+//PUT /quizes/:id/update
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz.validate().then(function(err){
+			if (err) {
+				res.render('quizes/edit', {quiz: quiz, errors: err.errors});
+			} else { //save: Guarda campos pregunta y respuesta de DB
+				req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+          res.redirect('/quizes');});
+			} // Redirección HTTP a lista de preguntas (URL relativo)
+		}
+	);
+};
+
 //GET /creditos
 exports.author = function(req, res){
   res.render('creditos', {errors: [] });
