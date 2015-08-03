076c3d07c1f7c2d43f6aa48ff0ccac0d37bd6ff7
fjvalverdep <fjvalverdep@hotmail.com>
2015-08-03 13:56:07 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-//router.get('/quizes/question', quizController.question);
-//router.get('/quizes/answer', quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
 
 /* Definición de rutas de /quizes */
 router.get('/quizes', quizController.index);
