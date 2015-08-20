a640f94ada9098239d12af57574129769c0d0019
fjvalverdep <fjvalverdep@hotmail.com>
2015-08-20 20:05:59 +0200

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login', sessionController.new); // formulario de login
@@ -28,6 +29,7 @@ router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizControl
 router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/creditos', quizController.author);
 
